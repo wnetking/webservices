edc2af5ae5e7b7b973f91195c4b4385fd09473bc
wnetking <wnetking>
2017-10-18 18:19:59 +0300

    small fix

--- src/modules/combinations/components/ImagePlaceholder.js
+++ src/modules/combinations/components/ImagePlaceholder.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import placeholder from 'img/product-placeholder.png'
 
 const ImagePlaceholder = () => {
   return (
-    <div></div>
+    <div>
+      <div className='siema loading'>
+        <img width='100%' src={placeholder} alt='Placeholder' />
+      </div>
+      <button className='prev' onClick={this.prev}>«</button>
+        <button className='next' onClick={this.next}>»</button>
+    </div>
   )
 }
 
-export default ImagePlaceholder;
+export default ImagePlaceholder
